e8b85f4f33e4bc995ea8d23654a02f6f62381c67
Yu <yuhuang.toronto@gmail.com>
2024-07-22 23:34:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #st.text(fruityvice_response.json())
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -54,6 +57,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    #st.text(fruityvice_response.json())
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
