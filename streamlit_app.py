f6c3004795f3229d525c797b8ef0d184cb7d1c0f
Yu <yuhuang.toronto@gmail.com>
2024-07-22 23:31:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import reqests
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
