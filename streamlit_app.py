ebfb0e9fa7200b057ae90b3f5985f4782a793c60
Yu <yuhuang.toronto@gmail.com>
2024-07-22 23:32:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,4 @@ if ingredients_list:
 
 
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response)
+    st.text(fruityvice_response.json())
