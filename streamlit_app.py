d88dd064979d1d2f04f8ff4674829d7b2fbff2f4
Yu <yuhuang.toronto@gmail.com>
2024-07-22 23:31:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import reqests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -52,33 +53,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-# # Get the current credentials
-# session = get_active_session()
 
-# # Use an interactive slider to get user input
-# hifives_val = st.slider(
-#     "Number of high-fives in Q3",
-#     min_value=0,
-#     max_value=90,
-#     value=60,
-#     help="Use this to enter the number of high-fives you gave in Q3",
-# )
-
-# #  Create an example dataframe
-# #  Note: this is just some dummy data, but you can easily connect to your Snowflake data
-# #  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
-# created_dataframe = session.create_dataframe(
-#     [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
-#     schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
-# )
-
-# # Execute the query and convert it into a Pandas dataframe
-# queried_data = created_dataframe.to_pandas()
-
-# # Create a simple bar chart
-# # See docs.streamlit.io for more types of charts
-# st.subheader("Number of high-fives")
-# st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
-
-# st.subheader("Underlying data")
-# st.dataframe(queried_data, use_container_width=True)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    st.text(fruityvice_response)
